584f1c94f9f23c1eb4d333950e835f34d8d5fbdd
alinili414e <alinili414e@gmail.com>
2021-11-18 09:14:56 -0500

    created auctions

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,7 +1,14 @@
 async function createAuction(event, context) {
+const body=JSON.parse(event.body);
+const now=new Date();
+const auction={
+  title:body.title,
+  status:'OPEN',
+  createdAt:now.toISOString(),
+};
   return {
-    statusCode: 200,
-    body: JSON.stringify({ event,context }),
+    statusCode: 201,
+    body: JSON.stringify(auction),
   };
 }
 
