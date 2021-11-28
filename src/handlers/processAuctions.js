86d8d691f606cbf0e30c7d31bdf48a1bf989ba5b
alinili414e <alinili414e@gmail.com>
2021-11-27 22:57:20 -0500

    added close auction functionaility

--- src/handlers/processAuctions.js
+++ src/handlers/processAuctions.js
@@ -1,5 +1,20 @@
+import { closeAuction } from '../lib/closeAuction';
+import { getEndedAuctions } from '../lib/getEndedAuctions'
+import createError from 'http-errors';
 
-async function processAuctions(event,context){
-console.log(`processing Auctions`);
+async function processAuctions(event, context) {
+
+    try {
+        const auctionsToClose = await getEndedAuctions();
+        const closePromises = auctionsToClose.map(auction => closeAuction(auction));
+        await Promise.all(closePromises);
+        return {closed:closePromises.length}
+    }
+    catch (e) {
+        console.error(e);
+        throw new createError.InternalServerError(e);
+    }
+
+
 }
-export const handler=processAuctions;
+export const handler = processAuctions;
