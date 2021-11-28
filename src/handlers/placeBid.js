cff1fe72cc41673b0f388eb870434f59585e07b0
alinili414e <alinili414e@gmail.com>
2021-11-27 23:02:59 -0500

    fixed a bug

--- src/handlers/placeBid.js
+++ src/handlers/placeBid.js
@@ -10,7 +10,9 @@ async function placeBid(event, context) {
   const { amount } = event.body;
 
   const auction = await getAuctionById(id);
-
+if(auction.status!=='OPEN'){
+  throw new createError.Forbidden('This Auction has been closed')
+}
   if (amount <= auction.highestBid.amount) {
     throw new createError.Forbidden(`Your bid must be higher than ${auction.highestBid.amount}!`);
   }
