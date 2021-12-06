67758b61c2dbcf35cf6e8da1aa53494cac921451
alinili414e <alinili414e@gmail.com>
2021-12-06 09:39:26 -0500

    added notification service

--- src/handlers/placeBid.js
+++ src/handlers/placeBid.js
@@ -10,8 +10,18 @@ const dynamodb = new AWS.DynamoDB.DocumentClient();
 async function placeBid(event, context) {
   const { id } = event.pathParameters;
   const { amount } = event.body;
+const {email}=event.requestContext.authorizer;
 
   const auction = await getAuctionById(id);
+if(email===auction.seller){
+  throw new createError.Forbidden('You cannot bid on your own auctions');
+}
+
+if(email===auction.highestBid.bidder ){
+  throw new createError.Forbidden('You are already the highest bidder');
+}
+
+
 if(auction.status!=='OPEN'){
   throw new createError.Forbidden('This Auction has been closed')
 }
@@ -22,9 +32,10 @@ if(auction.status!=='OPEN'){
   const params = {
     TableName: process.env.AUCTIONS_TABLE_NAME,
     Key: { id },
-    UpdateExpression: 'set highestBid.amount = :amount',
+    UpdateExpression: 'set highestBid.amount = :amount,highestBid.bidder=:bidder',
     ExpressionAttributeValues: {
       ':amount': amount,
+      ':bidder':email,
     },
     ReturnValues: 'ALL_NEW',
   };
